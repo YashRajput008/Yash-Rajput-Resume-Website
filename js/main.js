6b0f5955ded877b185fd7bf332965256c32d673b
Yash Rajput <toyashrajput@gmail.com>
2024-06-22 13:33:39 -0400

    Added leadership roles + cleaned up parts + added animation to a lot of the website

--- js/main.js
+++ js/main.js
@@ -1,31 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const slides = document.querySelectorAll(".slide");
-  const nextButton = document.getElementById("next");
-  const prevButton = document.getElementById("prev");
-  let currentIndex = 0;
-
-  const updateSlider = () => {
-    const slider = document.querySelector(".slider");
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-  };
-
-  nextButton.addEventListener("click", () => {
-    if (currentIndex < slides.length - 1) {
-      currentIndex++;
-    } else {
-      currentIndex = 0;
-    }
-    updateSlider();
-  });
-
-  prevButton.addEventListener("click", () => {
-    if (currentIndex > 0) {
-      currentIndex--;
-    } else {
-      currentIndex = slides.length - 1;
-    }
-    updateSlider();
-  });
-
-  updateSlider();
+document.addEventListener("DOMContentLoaded", function () {
+  const text = document.getElementById("hoverText");
+  const content = text.textContent;
+  text.innerHTML = content
+    .split("")
+    .map((letter) => `<span>${letter}</span>`)
+    .join("");
 });
